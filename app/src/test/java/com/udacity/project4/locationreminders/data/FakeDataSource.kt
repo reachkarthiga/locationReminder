926b62a7a1f7e54bc75e2d5effbe905df74e48c8
Karthiga <lkarthigalm@gmail.com>
2023-02-13 12:34:18 +0530

    Initial Testing Code

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,28 +1,32 @@
 package com.udacity.project4.locationreminders.data
-//
-//import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-//import com.udacity.project4.locationreminders.data.dto.Result
-//
-////Use FakeDataSource that acts as a test double to the LocalDataSource
-//class FakeDataSource : ReminderDataSource {
-//
-////    TODO: Create a fake data source to act as a double to the real data source
-//
-//    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-//        TODO("Return the reminders")
-//    }
-//
-//    override suspend fun saveReminder(reminder: ReminderDTO) {
-//        TODO("save the reminder")
-//    }
-//
-//    override suspend fun getReminder(id: String): Result<ReminderDTO> {
-//        TODO("return the reminder with the id")
-//    }
-//
-//    override suspend fun deleteAllReminders() {
-//        TODO("delete all the reminders")
-//    }
-//
 
-//}
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+
+
+//Use FakeDataSource that acts as a test double to the LocalDataSource
+class FakeDataSource (val list : MutableList<ReminderDTO>? = mutableListOf()): ReminderDataSource {
+
+//    Done: Create a fake data source to act as a double to the real data source
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+       list?.let {
+           return Result.Success(ArrayList(it))
+       }
+        return Result.Error("NO Reminders")
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        list?.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        TODO()
+    }
+
+    override suspend fun deleteAllReminders() {
+        list?.clear()
+    }
+
+
+}
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,13 +1,61 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
+
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var reminderDataSource: FakeDataSource
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun initialSetUp() {
+        reminderDataSource = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(ApplicationProvider.getApplicationContext(), reminderDataSource )
+    }
+
+    @Test
+    fun onClear_clearsLiveDataObjects() {
+        saveReminderViewModel.reminderTitle.value = "Place 1"
+        saveReminderViewModel.latitude.value = 13.23
+        saveReminderViewModel.longitude.value = 12.2
+        saveReminderViewModel.selectedPOI.value = PointOfInterest(LatLng(2.0,3.0), null, null)
+        saveReminderViewModel.reminderSelectedLocationStr.value = "Selected Place 1"
+        saveReminderViewModel.reminderDescription.value = "Place 1 Description"
+
+        saveReminderViewModel.onClear()
+
+        assertThat(saveReminderViewModel.reminderTitle.value , `is`(nullValue()))
+        assertThat(saveReminderViewModel.latitude.value , `is`(nullValue()))
+        assertThat(saveReminderViewModel.longitude.value , `is`(nullValue()))
+        assertThat(saveReminderViewModel.selectedPOI.value , `is`(nullValue()))
+        assertThat(saveReminderViewModel.reminderSelectedLocationStr.value , `is`(nullValue()))
+        assertThat(saveReminderViewModel.reminderDescription.value , `is`(nullValue()))
+
+    }
+
 
 
     //TODO: provide testing to the SaveReminderView and its live data objects
